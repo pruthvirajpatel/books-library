80d29d19b27aea5660fb49f51c76efc1e9d449fd
Pruthviraj Patel <pruthviraj@lendingkart.com>
2020-01-12 22:23:16 +0530

    Search module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from '../header/header.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, HeaderComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SearchRoutingModule
   ]
 })
